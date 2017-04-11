fcdf6b2fa26d9971ea1266b36869975b22d9fed5
Besancon <besancon.julien@gmail.com>
2017-04-11 14:55:54 +0200

    ajout de la fonction des logins : possibilité de se connecter et gère la possibilité de faire une action si on est connecté et si pas connecter demande l'authentification

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from .app import app,manager,db
+from .app import app,manager,db,login_manager
 import application.views
 import application.commands
 from .models import User,Album,Artiste,Genre,get_artistes,get_albums,get_genres,Album_Genre
--- application/app.py
+++ application/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 def mkpath(p):
     return os.path.normpath(os.path.join(os.path.dirname(__file__),p))
@@ -16,4 +16,6 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI']=('sqlite:///'+mkpath('../application/models/music.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-# app. config ['SECRET_KEY'] = "5598d6910d68379ae752d2dc58c919be"
+app. config ['SECRET_KEY'] = "5598d6910d68379ae752d2dc58c919be"
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
